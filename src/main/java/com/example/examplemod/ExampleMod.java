44c5e3cf7b133165cff59833c15be1c3501f9232
Hector G.T <deadreapergaming2846@gmail.com>
2016-11-12 17:46:13 -0400

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,38 @@
-package com.example.examplemod;
+package dantetc.buildersdelight;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+@Mod(
+        modid = BuildersDelight.MOD_ID,
+        name = BuildersDelight.MOD_NAME,
+        version = BuildersDelight.VERSION
+)
+public class BuildersDelight {
+
+    public static final String MOD_ID = "buildersdelight";
+    public static final String MOD_NAME = "Builders Delight";
+    public static final String VERSION = "1.1-SNAPSHOT";
+
+    @Mod.Instance BuildersDelight
+    public static BuildersDelight instance;
+
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    public void preinit(FMLPreInitializationEvent e) {
+        System.out.println("Called method: [preinit]");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        System.out.println("Called method: [init]");
+    }
+
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent e) {
+        System.out.println("Called method: [postinit]");
     }
 }
