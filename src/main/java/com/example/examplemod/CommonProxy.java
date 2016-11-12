8c872d85e9ca87f24327bfd479c6a42d6f2447f5
Hector G.T <deadreapergaming2846@gmail.com>
2016-11-12 17:50:26 -0400

    Create CommonProxy.java

---
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -0,0 +1,22 @@
+package dantetc.buildersdelight;
+
+import dantetc.buildersdelight.block.ModBlocks;
+import dantetc.buildersdelight.items.ModItems;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+        ModItems.createItems();
+        ModBlocks.createBlocks();
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
